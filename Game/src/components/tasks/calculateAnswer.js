1e122c33ec58b75f4f84705dc3c2645d2dae91a6
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-01-07 21:22:30 +0300

    *feat add task and logic task

---
+++ Game/src/components/tasks/calculateAnswer.js
@@ -0,0 +1,17 @@
+﻿export default function calculateAnswer() {
+  const task = document.querySelector('#task-to-solve');
+  const operator = ['-', '+', '/', '*'];
+  let expression = '1.5';
+  while ((eval(expression) ^ 0) !== eval(expression)) {
+    expression = `${Math.floor(Math.random() * 60)}`
+          + ` ${operator[Math.floor(Math.random() * 4)]} `
+          + `${Math.floor(Math.random() * 9 + 1)}`;
+  }
+  task.innerHTML = `<div class="task__condition">
+      <p>Решите пример:</p>
+      <p class = "conditionTask">${expression} =</p> <input autofocus class = "task-1" style = "width: 60px" type="text" >   
+     </div>
+     <input id = "task1__button"  class="task__button" type="button" value="Ответить">`;
+  document.querySelector('.task-1').focus();
+  return eval(expression);
+}
